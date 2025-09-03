db27814bec22b53c57ca365a2a3b73abeea9b157
Oskar-Sobala <oskars0318@gmail.com>
2025-09-03 12:37:18 +0200

    klar med miniräknaren

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function addNumbers(a, b) {
     return a + b;
 }
 
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+function multipliceraNumbers(a, b){
+    return a*b;
+}
+
+function divideNumbers(a, b){
+    return a/b;
+}
+
 // STEG 2: Globala variabler (bara för addition nu)
 let firstNumber = '';
 let operation = '';
@@ -49,14 +61,24 @@ function handleOperator(op) {
 
 // STEG 7: Räkna ut (bara addition)
 function calculate() {
-    if (firstNumber !== '' && operation === '+' && secondNumber !== '') {
-        // PROBLEM: firstNumber och secondNumber är strings! 
-        // Vad händer när du adderar "1" + "2"?
-        const result = addNumbers(firstNumber, secondNumber);
-
+    if (firstNumber !== '' && secondNumber !== '') {
+        const num1 = Number(firstNumber);
+        const num2 = Number(secondNumber);
+        let result;
+        
+        if (operation === '+') {
+            result = addNumbers(num1, num2);
+        } else if (operation === '-') {
+            result = subtractNumbers(num1, num2);  // Du behöver skriva denna funktion!
+        } else if (operation === '*') {
+            result = multipliceraNumbers( num1, num2);
+        } else if (operation === '/') {
+            result = divideNumbers(num1, num2);
+        }
+        
         displayValue = result;
         updateDisplay();
-
+        
         // Återställ
         firstNumber = '';
         operation = '';
@@ -64,6 +86,16 @@ function calculate() {
         updateDebug();
     }
 }
+
+function clearAll() {
+    firstNumber = '';
+    operation = '';
+    secondNumber = '';
+    displayValue = '0';
+    updateDisplay();
+    updateDebug();
+}
+
 
 // STEG 8: Event listeners (bara för knapparna vi har)
 document.getElementById('btn-1').addEventListener('click', function () {
@@ -74,13 +106,62 @@ document.getElementById('btn-2').addEventListener('click', function () {
     handleNumber('2');
 });
 
-document.getElementById('btn-plus').addEventListener('click', function () {
+document.getElementById('btn-3').addEventListener('click', function () {
+    handleNumber('3');
+});
+
+document.getElementById('btn-4').addEventListener('click', function () {
+    handleNumber('4');
+});
+
+document.getElementById('btn-5').addEventListener('click', function () {
+    handleNumber('5');
+});
+
+document.getElementById('btn-6').addEventListener('click', function () {
+    handleNumber('6');
+});
+
+document.getElementById('btn-7').addEventListener('click', function () {
+    handleNumber('7');
+});
+
+document.getElementById('btn-8').addEventListener('click', function () {
+    handleNumber('8');
+});
+
+document.getElementById('btn-9').addEventListener('click', function () {
+    handleNumber('9');
+});
+
+document.getElementById('btn-0').addEventListener('click' , function () {
+    handleNumber('0');
+})
+
+
+document.getElementById('btn-minus').addEventListener('click',  () => {
+    handleOperator('-')
+});
+
+document.getElementById('btn-plus').addEventListener('click',  () => {
     handleOperator('+');
 });
 
-document.getElementById('btn-equals').addEventListener('click', function () {
+document.getElementById('btn-equals').addEventListener('click',  () => {
     calculate();
 });
+
+document.getElementById('btn-clear').addEventListener('click', () => {
+    clearAll();
+});
+
+document.getElementById('btn-multiply').addEventListener('click', () => {
+    handleOperator('*');
+})
+
+document.getElementById('btn-divide').addEventListener('click',  () => {
+    handleOperator('/');
+})
 
 // TESTOMRÅDE
 console.log('Kalkylator laddad!');
